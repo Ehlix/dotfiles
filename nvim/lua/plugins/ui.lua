a0d001b12cfe36791ec6fdb4b4fff374ddbab2a6
helix <bozardhh@hotmail.com>
2024-11-19 09:22:43 +0300

    nvim upd

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,44 +1,4 @@
-local Util = require("lazyvim.util")
-
--- like lazyvim.util.lualine.pretty_path but with a longer path (3 -> 6)
----@param opts? {relative: "cwd"|"root", modified_hl: string?}
-local function lualine_pretty_path(opts)
-  opts = vim.tbl_extend("force", {
-    relative = "cwd",
-    modified_hl = "Constant",
-  }, opts or {})
-
-  return function(self)
-    local path = vim.fn.expand("%:p") --[[@as string]]
-
-    if path == "" then
-      return ""
-    end
-    local root = Util.root.get({ normalize = true })
-    local cwd = Util.root.cwd()
-
-    if opts.relative == "cwd" and path:find(cwd, 1, true) == 1 then
-      path = path:sub(#cwd + 2)
-    else
-      path = path:sub(#root + 2)
-    end
-
-    local sep = package.config:sub(1, 1)
-    local parts = vim.split(path, "[\\/]")
-    if #parts > 6 then
-      parts = { parts[1], parts[2], parts[3], parts[4], "…", parts[#parts - 1], parts[#parts] }
-    end
-
-    if opts.modified_hl and vim.bo.modified then
-      parts[#parts] = Util.lualine.format(self, parts[#parts], opts.modified_hl)
-    end
-
-    return table.concat(parts, sep)
-  end
-end
-
 return {
-
   -- edgy
   {
     "folke/edgy.nvim",
@@ -79,18 +39,18 @@ return {
       },
     },
     keys = {
-      { "<space>t", "<Cmd>BufferLinePick<CR>", desc = "Buffer picker" },
-      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle pin" },
+      { "<space>t",   "<Cmd>BufferLinePick<CR>",                 desc = "Buffer picker" },
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle pin" },
       { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete non-pinned buffers" },
-      { "<leader>1", "<Cmd>BufferLineGoToBuffer 1<CR>" },
-      { "<leader>2", "<Cmd>BufferLineGoToBuffer 2<CR>" },
-      { "<leader>3", "<Cmd>BufferLineGoToBuffer 3<CR>" },
-      { "<leader>4", "<Cmd>BufferLineGoToBuffer 4<CR>" },
-      { "<leader>5", "<Cmd>BufferLineGoToBuffer 5<CR>" },
-      { "<leader>6", "<Cmd>BufferLineGoToBuffer 6<CR>" },
-      { "<leader>7", "<Cmd>BufferLineGoToBuffer 7<CR>" },
-      { "<leader>8", "<Cmd>BufferLineGoToBuffer 8<CR>" },
-      { "<leader>9", "<Cmd>BufferLineGoToBuffer 9<CR>" },
+      { "<leader>1",  "<Cmd>BufferLineGoToBuffer 1<CR>" },
+      { "<leader>2",  "<Cmd>BufferLineGoToBuffer 2<CR>" },
+      { "<leader>3",  "<Cmd>BufferLineGoToBuffer 3<CR>" },
+      { "<leader>4",  "<Cmd>BufferLineGoToBuffer 4<CR>" },
+      { "<leader>5",  "<Cmd>BufferLineGoToBuffer 5<CR>" },
+      { "<leader>6",  "<Cmd>BufferLineGoToBuffer 6<CR>" },
+      { "<leader>7",  "<Cmd>BufferLineGoToBuffer 7<CR>" },
+      { "<leader>8",  "<Cmd>BufferLineGoToBuffer 8<CR>" },
+      { "<leader>9",  "<Cmd>BufferLineGoToBuffer 9<CR>" },
     },
   },
 
@@ -106,7 +66,7 @@ return {
           end,
         },
       }
-      opts.sections.lualine_c[4] = { lualine_pretty_path() }
+      opts.sections.lualine_c[4] = { LazyVim.lualine.pretty_path({ length = 6 }) }
       -- move metals status to the left
       opts.sections.lualine_z = opts.sections.lualine_y
       opts.sections.lualine_y = opts.sections.lualine_x
@@ -118,9 +78,12 @@ return {
 
   -- dashboard
   {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      local logo = [[
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        width = 114,
+        preset = {
+          header = [[
 ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 ░░░░░░░░░░░░░▒▓▒▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▓▓▓▓▓▓▓▓▒▒▒▓▒░░░░░░░░░░
 ░░░░░░▒▓▓█▓▓██████████████▓▓▓▓▓▒▒▒▒░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▒▒▒▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓█▓░░░░░░░
@@ -138,9 +101,15 @@ return {
 ████████████████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒░░░░░                 ░░░░░▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████████████████
 ████████████████████▓▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒░░░░░  Z A Z A V I M  ░░░░░▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓████████████████████
       ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-    end,
+        }
+      }
+    },
+    sections = {
+      { section = "header" },
+      { section = "keys",   gap = 1, padding = 1 },
+      { section = "startup" },
+    },
+    -- header = vim.split(logo, "\n")
   },
 }
 -- local logo = [[
